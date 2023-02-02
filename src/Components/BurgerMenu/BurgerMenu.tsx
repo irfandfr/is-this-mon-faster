d7e87cc921dbd4c7ce965bfc8f69bf8736c3a135
Irfandhia Firdaus <irfandfr@gmail.com>
2023-02-03 02:02:57 +0700

    add header navs

--- src/Components/BurgerMenu/BurgerMenu.tsx
+++ src/Components/BurgerMenu/BurgerMenu.tsx
@@ -2,13 +2,14 @@ import styles from './burgermenu.module.scss'
 
 interface menuProps{
   open: boolean
+  className?: string
   setOpen?: () => void;
   style?: React.CSSProperties;
 }
 
-const BurgerMenu = ({open, setOpen, style} : menuProps) =>{
+const BurgerMenu = ({open, className, setOpen, style} : menuProps) =>{
   return(
-    <button className={`${styles.burgerMenu} ${open ? styles.open: ''}`} style={style} onClick={setOpen}>
+    <button className={`${styles.burgerMenu} ${!!className ? className : ''} ${open ? styles.open: ''}`} style={style} onClick={setOpen}>
       <div className={styles.barContainer}>
         <div className={styles.bar}></div>
         <div className={styles.bar}></div>
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -10,11 +10,20 @@ const Header = () =>{
   }
   return(
     <header className={style.header}>
-      <a href="/" className={style.appLogoName}>
-        <img src="/isMonFasterLogo.svg" alt="isMonFaster Logo" style={{height: '30px'}}/>
-        <span className={style.headerAppName}> isMonFaster?</span> 
-      </a>
-      <BurgerMenu open={open} setOpen={toggleMenu} style={{marginLeft: 'auto'}}/>
+      <div className={style.headerContent}>
+        <a href="/" className={style.appLogoName}>
+          <img src="/isMonFasterLogo.svg" alt="isMonFaster Logo" className={style.isMonLogo}/>
+          <span className={style.headerAppName}> isMonFaster?</span> 
+        </a>
+        <BurgerMenu className={style.menuBtn} open={open} setOpen={toggleMenu} />
+        <nav className={`${style.navContainer} ${open ? style.open : ''}`}>
+          <ul>
+            <li className={style.active}>Home</li>
+            <li>Calculator</li>
+            <li>Advanced Calculator</li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
