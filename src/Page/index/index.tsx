f9d038e4485a913cc51478cbb9e7b344ee44f473
Irfandhia Firdaus <irfandfr@gmail.com>
2023-02-02 14:34:00 +0700

    add dark style theme

--- src/Page/index/index.tsx
+++ src/Page/index/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../../Components/Button/Button'
 import Card from '../../Components/Card/Card'
 import BigInput from '../../Components/Forms/BigInput/BigInput'
 
@@ -7,9 +8,9 @@ import style from './homepage.module.scss'
 const Homepage = () =>{
   return(
     <main className={style.homepage}>
-      <h3 className={style.textIs}>Is</h3>
+      <h3 className={style.text} style={{margin: '33px 0'}}>Is</h3>
       <BigInput color='#36B7AF'/>
-      <h4 style={{textAlign: 'center', margin: '34px 0'}}>faster <br /> than</h4>
+      <h4 className={style.text}style={{textAlign: 'center', margin: '34px 0'}}>faster <br /> than</h4>
       <BigInput color='#9221C7'/>
       <Card style={{marginTop: '37px'}}>
         <Card.Header text='Find Out:' />
@@ -19,6 +20,7 @@ const Homepage = () =>{
         <Card.ListItem text='Potential risk (Choice Scarf, Tailwind)' type='warning' />
         <Card.ListItem text='Minimum speed boost to outspeed or outsped' type='warning' />
       </Card>
+      <Button text='Try Now' type='primary' style={{paddingLeft: '20px', paddingRight: '20px', marginTop: '37px', marginBottom: '20px'}}/>
     </main>
   )
 }
