55f2f2711c29780ea15c06ba8b48c31165df347c
Irfandhia Firdaus <irfandfr@gmail.com>
2023-02-22 17:28:10 +0700

    add link type button

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -6,19 +6,29 @@ interface ButtonProp{
   iconLink?: string
   icon?: ReactElement
   disabled? : boolean
-  type? : 'primary' | 'secondary' | 'danger'
+  type? : 'primary' | 'secondary' | 'danger' 
+  href? : string
   style? : React.CSSProperties
   onClick?: (e:React.MouseEvent<HTMLButtonElement>) => void | ((e:React.MouseEvent<HTMLButtonElement>) => {})
 }
 
-const Button = ({text, icon, disabled, type, style, onClick}: ButtonProp) =>{
+const Button = ({text, icon, disabled, type, style, href, onClick}: ButtonProp) =>{
   
-  return(
-    <button className={`${styles.button} ${!!icon && styles.iconed} ${!!type ? styles[type] : ''}`} disabled={disabled} style={style} onClick={onClick}>
-      {icon && React.cloneElement(icon, {className: `${icon.props.className} ${styles.icon}`})}
-      {text}
-    </button>
-  )
+  if(!!href){
+    return(
+      <a href={!!href ? href : '#'} className={`${styles.button} ${!!icon && styles.iconed} ${!!type ? styles[type] : ''}`} style={style}>
+        {icon && React.cloneElement(icon, {className: `${icon.props.className} ${styles.icon}`})}
+        {text}
+      </a>
+    )
+  }else{
+    return(
+      <button className={`${styles.button} ${!!icon && styles.iconed} ${!!type ? styles[type] : ''}`} disabled={disabled} style={style} onClick={onClick}>
+        {icon && React.cloneElement(icon, {className: `${icon.props.className} ${styles.icon}`})}
+        {text}
+      </button>
+    )
+  }
 }
 
 
