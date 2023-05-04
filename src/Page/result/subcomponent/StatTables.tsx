eded2c71938d57a483baaf06eb70f86b763ab0ec
Irfandhia Firdaus <irfandfr@gmail.com>
2023-05-04 09:42:26 +0700

    integrate stattables w/ api + add mod icons

--- src/Page/result/subcomponent/StatTables.tsx
+++ src/Page/result/subcomponent/StatTables.tsx
@@ -1,6 +1,8 @@
+import { useRef } from "react"
 import Card from "../../../Components/Card/Card"
 import statCalculator from "../../../Utils/statCalculator"
 import { Modifiers } from "../../../Utils/types"
+import RenderModifiers from "../RenderModifier"
 
 import style from './../result.module.scss'
 
@@ -22,6 +24,7 @@ const StatTables = ({p1base,p2base,p1mods,p2mods,p2ImgLink,p1ImgLink}:StatTables
     {title: 'MinEVIV', ev: 0, iv:0, nature:'neutral'},
     {title: 'MinEVIV-', ev: 0, iv:0, nature:'hindering'},
   ]
+
   function rendertables(){
     return(
       tableContent.map((content) => {
@@ -36,10 +39,25 @@ const StatTables = ({p1base,p2base,p1mods,p2mods,p2ImgLink,p1ImgLink}:StatTables
     )
   }
   return(
-    <Card>
+    <Card className={style.tableCard}>
       <Card.Header text="Stat Calculations" />
       <table className={style.statTable}>
         <tbody>
+          <tr>
+            <th></th>
+            <th>
+              <div className={style.pkmnContainer}>
+                <img className={style.sprite} src={p1ImgLink} alt="Pokemon1's Sprite" loading="lazy" />
+                <RenderModifiers mods={p1mods} pnumber={1} size='s'/>
+              </div>
+            </th>
+            <th>
+              <div  className={style.pkmnContainer}>
+                <img className={style.sprite} src={p2ImgLink} alt="Pokemon1's Sprite" loading="lazy" />
+                <RenderModifiers mods={p2mods} pnumber={2} size='s'/>
+              </div>
+            </th>
+          </tr>
           <tr>
             <th>Base Speed</th>
             <th className={style.p1text} style={{fontWeight: '600'}}>{p1base}</th>
